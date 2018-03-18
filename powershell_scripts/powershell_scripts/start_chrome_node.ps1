8843aea6a27c25066260f202cc8bbea303227d70
sglezcasco <36644895+sglezcasco@users.noreply.github.com>
2018-03-18 23:56:01 +0100

    update download jenkins powershell script

---
+++ powershell_scripts/powershell_scripts/start_chrome_node.ps1
@@ -0,0 +1,4 @@
+﻿# Run as ADMIN
+java -version
+cd C:\Users\sergiog\Desktop\powershell_scripts
+java '-Dwebdriver.chrome.driver=C:\\SoftDesarrollo\\SeleniumWebdriver\\chromedriver.exe' -jar C:\\SoftDesarrollo\\SeleniumGrid\selenium-server-standalone-3.7.1.jar -role node -hub http://192.168.43.247:4444/grid/register -browser "browserName=chrome,platform=WINDOWS,maxInstances=1"
