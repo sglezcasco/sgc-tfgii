155cb34e5cdd8a18f9aad5e6af95a65554ffaaf1
sglezcasco <sergiogonzalezcasco@gmail.com>
2018-03-22 23:40:17 +0100

    update paths in windows powershell scripts

---
+++ powershell_scripts/install_phantomjs.ps1
@@ -0,0 +1,5 @@
+﻿# Run as ADMIN
+#GeckoDriver
+mkdir "C:/SoftDesarrollo/SeleniumWebdriver2"
+Invoke-WebRequest "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-windows.zip" -OutFile "C:/SoftDesarrollo/SeleniumWebdriver2/phantomjs-1_9_7-windows.zip" ;
+Expand-Archive "C:/SoftDesarrollo/SeleniumWebdriver2/phantomjs-1_9_7-windows.zip"  -DestinationPath "C:/SoftDesarrollo/SeleniumWebdriver2" ;  
